e168a736c76ccd73c4c2155c00953cfbeead2ebe
Lai Weng Han <jlwhoo7@gmail.com>
2018-11-16 11:52:50 +0800

    uncommented recursion in rs

---
+++ automated.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# AUTOMATED SCRIPT
+
+# TODO: Add Recusrion
--- src/01_recursion/rs/index.rs
+++ src/01_recursion/rs/index.rs
@@ -1,11 +1,10 @@
 // Recursion
 
-// fn factorial(value: i64) -> i64 {
-//     return if value == 1 { value } else { value * factorial(value - 1) };
-// }
+fn factorial(value: i64) -> i64 {
+    return if value == 1 { value } else { value * factorial(value - 1) };
+}
 
 fn main() {
-    // let a = factorial(3);
-    let a = 3;
+    let a = factorial(3);
     println!("data: {}", a);
 }
